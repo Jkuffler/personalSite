263c7c7d8cbc440a1147d43dcf418b5c6cabc20a
Jason <jakuffler@gmail.com>
2021-07-11 10:20:36 -0400

    i should do more commits

---
+++ index.js
@@ -0,0 +1,5 @@
+const clicker = document.getElementById('clicker');
+
+clicker.addEventListener('click', function() {
+    alert('I\'m not often referred to as Jason. Use Kuffler for quickest response! Thanks :)')
+})
